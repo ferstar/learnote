670f5b714c9e5dee3d1eb867337a70e2f8e7ca2e
Jason Chang <crazycashier@icoud.com>
2016-12-18 21:53:00 +0800

    update 内网穿透

--- ngrok内网穿透_openVPN/README.md
+++ ngrok内网穿透_openVPN/README.md
@@ -1,12 +1,22 @@
-# ngrok 配合 openVPN做内网穿透, 实现远程访问内网服务器的目的
+# ngrok 内网穿透
+
+最早在知乎看到这个问题, 不想自己也碰到了同样的尴尬
+
+[如何给不能做端口映射的内网架设VPN](https://www.zhihu.com/question/31220460/answer/103939079)
+
+这里不会再重复我在知乎的回答, 只讲一种后来我在用的新方法, 效果很棒
 
 用到的软件有三
 
-1. 类ngrok的软件natapp
-2. openVPN
-3. supervisor
+1. 类ngrok的软件[natapp](https://natapp.cn/)
+2. [openVPN](https://openvpn.net/)
+3. [supervisor](http://supervisord.org/)
 
 
-由于用的是免费版本, 端口会不定期变动, 所以写了脚本, 检测到端口发生变化时, 给我发邮件/零信消息
+具体思路: 用natapp做内网穿透, 在内网机器上部署openVPN server, 用supervisor监控natapp进程状态, 这样即实现在外网轻松访问内网资源的需求, 简单描述为 VPN over ngrok under supervisor control.
+
+教程: [natapp(ngrok) Linux 下使用 supervisor 后台运行&开机启动](https://natapp.cn/article/supervisor)
+
+由于我用的是免费版本, 端口会不定期变动, 所以写了脚本, 检测到端口发生变化时, 给我发邮件/零信消息
 
 目前使用良好
--- ngrok内网穿透_openVPN/check_ngrok_port.sh
+++ ngrok内网穿透_openVPN/check_ngrok_port.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 export PATH=/bin:/usr/bin
 
+# 从最新日志中抓取natapp最新端口
 new_port=$(grep "Tunnel established at" /var/log/supervisor/natapp_stdout.log | tail -1 | awk -F: '{print $5}')
+# 从历史日志中抓取natapp服务端口
 if [ ! new_port ]; then
     new_port=$(grep "Tunnel established at" /var/log/supervisor/natapp_stdout.log* | tail -1 | awk -F: '{print $6}') 
 fi
 
 if [ ! -f /tmp/ngrok_port ]; then
-    # echo -e "create ngrok_port file"
     echo $new_port > /tmp/ngrok_port
 fi
 
 old_port=$(cat /tmp/ngrok_port)
 
 if [ $old_port != $new_port ]; then
-    #curl -s -X "POST" "https://hooks.pubu.im/services/pz98n941ngymfww" -H "Content-Type: application/json" -d "{\"snippet\":{\"type\":\"shell\",\"code\":\"$new_port\",\"name\":\"端口变更\"}}" > /dev/null
+    # 检测到端口变更, 给零信发送incoming message
     curl -s https://hooks.pubu.im/services/xxx -F text=$new_port > /dev/null
+    # 向目标邮箱发送变更后的端口(要求本机正确配置了mail服务)
     echo $new_port | mail -s "VPN port has been changed" fer_star@qq.com
+    # 暂存新端口
     echo $new_port > /tmp/ngrok_port
 fi
